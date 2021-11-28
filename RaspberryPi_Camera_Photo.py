ebebbd45aa8a999eca5449ace47a131ab8bb505d
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-11-28 08:07:14 +0100

    Caputer+Comments Added

--- RaspberryPi_Camera_Photo.py
+++ RaspberryPi_Camera_Photo.py
@@ -1,11 +1,11 @@
 #This Program will run on your Raspberry Pi
 #For this to work , you have to enable Camera via the Raspberry pi System Configuration
-#Access System Configuration on the CLI using "sudo raspi-config"
+#Access "System Configuration" on the CLI using the command  "sudo raspi-config"
 
 from picamera import PiCamera
 import time
 
 camera=PiCamera()
-camera.resolution=(1920,1080)
-camera.vflip=True
-camera.capture("FistTake.jpg")
+camera.resolution=(1920,1080) # Raspberry pI configureation is set here 
+camera.vflip=True # this flip the camera view vertically 
+camera.capture("FistTake.jpg") # This line take a snapshot once. (At the instant the camera is started)
