62eaa2bfe58c4b572c79173014e5366b46a1fca5
Ore-Oluwa <75027292+Oreoluwa-IVT@users.noreply.github.com>
2021-11-28 07:55:14 +0100

    RaspberryPi Camera Setup

---
+++ RaspberryPi_Camera_Photo.py
@@ -0,0 +1,11 @@
+#This Program will run on your Raspberry Pi
+#For this to work , you have to enable Camera via the Raspberry pi System Configuration
+#Access System Configuration on the CLI using "sudo raspi-config"
+
+from picamera import PiCamera
+import time
+
+camera=PiCamera()
+camera.resolution=(1920,1080)
+camera.vflip=True
+
