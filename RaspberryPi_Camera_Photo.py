28edd12a8fbb8406be1eb6c7d53d865822c2a63c
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-12-14 13:21:19 +0100

    Updated

--- RaspberryPi_Camera_Photo.py
+++ RaspberryPi_Camera_Photo.py
@@ -2,15 +2,33 @@
 #For this to work , you have to enable Camera via the Raspberry pi System Configuration
 #Access "System Configuration" on the CLI using the command  "sudo raspi-config"
 
+#Python Script for UAV Video Streaming
+import io
+import threading , os, signal
+import picamera
 from picamera import PiCamera
+import socketserver
+#from evdev import input InputDevice
+from select import select
+from threading import Condition
+from http import server
+import threading , os , signal
+import subprocess
+from subprocess import check_call , call
+import sys
+import glob
 import time
 
-camera=PiCamera()
-camera.resolution=(1920,1080) # Raspberry pI configureation is set here 
-camera.vflip=True # this flip the camera view vertically 
-camera.capture("FistTake.jpg") # This line take a snapshot once. (At the instant the camera is started)
-camera.stat_preview()
-camera.capture("Ayeoluwa.jpg")
-camera.start_recording("newvideo.h264")
-time.sleep(40) #it takes the video for 40secs then stops 
-camera.stop_recording()
+uavcam= PiCamera()
+uavcam.resolution=(1920,1080)
+uavcam.vflip=True
+uavcam.start_preview()
+uavcam.capture("/home/pi/Downloads/TestCamShoot.jpg") #this photo saved in the default
+
+#script location
+uavcam.start_recording("/home/pi/Downloads/TestCamVid.h264")
+time.sleep(70) #Video will record for 20 seconds
+uavcam.stop_recording()
+#cd Documents
+#omxplayer "videofilename"
+#use omxplayer to play the h264 video 
